6d19bb6287e2700693f9c94eb1b59cabbc0f880d
Deborah Wyny <deborahwyny25@gmail.com>
2023-04-07 16:34:28 -0300

    prompt finalizado

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log(oi)
+
+function escolhaCartas() {
+    let qtdCartas = prompt('Insira um numero par de 2 a 14 para iniciar o jogo:');
+    while(qtdCartas < 2 || qtdCartas > 14) {
+        qtdCartas = prompt('Insira um numero par de 2 a 14 para iniciar o jogo:');
+    }
+    return qtdCartas;
+}
+
+const qtdCartas = escolhaCartas();
